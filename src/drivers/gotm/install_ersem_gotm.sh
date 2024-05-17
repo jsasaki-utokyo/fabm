695ac3e4c79aeb8facc3545f20f00c7a02eeb0b5
Jun Sasaki <jsasaki.ece@gmail.com>
2024-05-18 01:03:12 +0900

    Initial commit

---
+++ src/drivers/gotm/install_ersem_gotm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# install_ersem_gotm.sh
+
+CMAKE_FLAG=""
+while getopts ":f:" flag; do
+    case "${flag}" in
+        f) CMAKE_FLAG=${OPTARG};;
+    esac
+done
+
+CPU="$(nproc)"
+FABM_HOST=gotm
+# Select ifort or gfortran
+CMAKE_Fortran_COMPILER=ifort
+# FFLAGS=-O3
+GOTM_BASE=~/Github/gotm/code
+FABM_ERSEM_BASE=~/Github/ersem
+FABM_BASE=~/Github/fabm/
+CMAKE_INSTALL_PREFIX=~/local/fabm-${CMAKE_Fortran_COMPILER}/${FABM_HOST}
+
+echo "Building GOTM-FABM-ERSEM"
+rm -rf ~/build  # Delete if exits.
+mkdir ~/build && cd ~/build
+cmake $GOTM_BASE -DFABM_BASE=$FABM_BASE -DFABM_ERSEM_BASE=$FABM_ERSEM_BASE -DCMAKE_Fortran_COMPILER=$CMAKE_Fortran_COMPILER -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE $CMAKE_FLAG
+make install -j $CPU
